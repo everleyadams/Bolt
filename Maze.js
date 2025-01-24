5f05c365f72034c981bc45dde9a0e78b6f41da83
everleyadams <everleyadams567@gmail.com>
2025-01-24 05:06:33 -0800

    Int maze.js Program

---
+++ Maze.js
@@ -0,0 +1,84 @@
+//Team Members: Evan Smith & Everley Adams
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #CF6D
+
+//Begin Program
+async function startProgram() {
+
+//Start and Say Start
+//Bolt will say Start acrossed the LED's & say it out loud
+//Driver:Everley
+//Navigator:Evan
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver:Evan
+//Navigator:Everley
+
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver:Everley
+//Navigator:Evan
+
+
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver:Evan
+//Navigator:Everley
+
+
+
+
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver:Everley
+//Navigator:Evan
+
+
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver:Evan
+//Navigator:Everley
+
+
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver:Everley
+//Navigator:Evan
+
+
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver:Evan
+//Navigator:Everley
+
+
+
+//Finished and Say Finished
+//Bolt will say Finished acrossed the LED's & say it out loud
+//Driver:Everley
+//Navigator:Evan
+
+
+
+
+//End Program
+}
