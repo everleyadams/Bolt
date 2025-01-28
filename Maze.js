991affffd5a001c1ac1c18a6b637307a0ab37bf9
everleyadams <everleyadams567@gmail.com>
2025-01-28 04:50:35 -0800

    Sound #1 Checkpoint

--- Maze.js
+++ Maze.js
@@ -21,8 +21,8 @@ async function startProgram() {
 //Driver:Evan
 //Navigator:Everley
 
-	setSpeed(63);
-	await delay (2.5);
+	setSpeed(39);
+	await delay (4.6);
 	setSpeed(0);
 	setMainLed({ r: 0, g: 255, b: 0 });
 	setSpeed(0);
@@ -35,9 +35,10 @@ async function startProgram() {
 //Driver:Everley
 //Navigator:Evan
 
-	await roll((getHeading() + 90), 49, 2.1);
+	await roll((getHeading() + 90), 49, 2.2);
 	setSpeed(0);
-	await roll((getHeading() + 90), 43, 1.1);
+	await delay (1);
+	await roll((getHeading() + 90), 44, 1.1);
 	await Sound.Animal.Monkey.play(true);
 
 
@@ -45,53 +46,6 @@ async function startProgram() {
 //LED's turn Red
 //Driver:Evan
 //Navigator:Everley
-
-
-
-
-
-
-//Sound #2 Checkpoint
-//Bolt makes sound of choice - Update Sound
-//Driver:Everley
-//Navigator:Evan
-
-
-
-
-//Purple Checkpoint
-//LED's turn Purple
-//Driver:Evan
-//Navigator:Everley
-
-
-
-
-//Sound #3 Checkpoint
-//Bolt makes sound of choice - Update Sound
-//Driver:Everley
-//Navigator:Evan
-
-
-
-
-//Blue Checkpoint
-//LED's turn Blue
-//Driver:Evan
-//Navigator:Everley
-
-
-
-//Finished and Say Finished
-//Bolt will say Finished acrossed the LED's & say it out loud
-//Driver:Everley
-//Navigator:Evan
-
-
-
-
-//End Program
-}
 
 
 
